70c3b7f275d0af4085db77e6b1a2cc2383bd3d3f
fvargas88 <fvargas_88@hotmail.com>
2023-02-02 07:15:53 -0300

    Mostrando subpastas a partir da past temp

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter a path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File :: isDirectory);
+		System.out.println("FOLDERS: ");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		
+	
+		
+		
+		
+		sc.close();
+
+	}
+
+}
